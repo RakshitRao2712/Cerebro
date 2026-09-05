d9f75738108bde4e892b2f03430d2d08d235e547
RakshitRao2712 <kirankrish2019@gmail.com>
2026-09-05 23:51:46 +0530

    tiny

--- src/main/java/com/example/Cerebro/controller/GeneratorTestController.java
+++ src/main/java/com/example/Cerebro/controller/GeneratorTestController.java
@@ -59,7 +59,7 @@ public class GeneratorTestController {
         return terraformValidateStrategy.validate(workspace);
     }
 
-    @GetMapping("/generate-write-validateevaluate")
+    @GetMapping("/generate-writevalidateevaluate")
     public EvalResult testEvaluate(@RequestParam String prompt) {
         GeneratedArtifact artifact = generatorAgent.generate(prompt, null, ToolEnum.TERRAFORM);
         Path workspace = fileIOService.createWorkspace();
